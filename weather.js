b92117578cd2c50fc838256853b7349213973742
briansamu <brian@xonndevelopment.com>
2020-08-30 10:25:46 -0400

    changed api fetch url http to https trying to fix error

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state},US&units=imperial&appid=${this.apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state},US&units=imperial&appid=${this.apiKey}`);
 
     const responseData = await response.json();
 
